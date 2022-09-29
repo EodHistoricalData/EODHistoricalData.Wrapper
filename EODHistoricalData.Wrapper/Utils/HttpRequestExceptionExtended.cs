f305fdd589263f9961dfd3223824272fa20152ad
Виталий Комышев <vkomyshev@micro-solution.ru>
2022-09-29 12:56:05 +0300

    added custom exception type

---
+++ EODHistoricalData.Wrapper/Utils/HttpRequestExceptionExtended.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace EODHistoricalData.Wrapper.Utils
+{
+    /// <summary>
+    /// Custom class for exceptions thrown in BaseAPI
+    /// </summary>
+    public class HttpRequestExceptionExtended : HttpRequestException
+    {
+        /// <summary>
+        /// Initializes a new instance of the HttpRequestExceptionExtended class with a specific message that describes the current exception and HTTP status code.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="statusCode"></param>
+        public HttpRequestExceptionExtended(string message, HttpStatusCode? statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public HttpStatusCode? StatusCode
+        {
+            get => _statusCode;
+            set
+            {
+                _statusCode = value;
+            }
+        }
+        private HttpStatusCode? _statusCode;
+    }
+}

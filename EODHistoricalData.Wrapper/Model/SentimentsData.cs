409bdd9a10deb2253ed6201e641a433416278870
Виталий Комышев <vkomyshev@micro-solution.ru>
2023-03-13 20:09:30 +0300

    [+] sentiments (-tweets) API [fix] options api url generator

---
+++ EODHistoricalData.Wrapper/Model/SentimentsData.cs
@@ -0,0 +1,31 @@
+﻿using EOD.Utils;
+using Newtonsoft.Json;
+using System;
+
+namespace EOD.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SentimentsData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("Date")]
+        private string DateJSON { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("none0")]
+        public DateTime? Date { get => GetDate.ParseDate(DateJSON); }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Count { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double? Normalized { get; set; }
+    }
+}
